4ac80ef24dfdcd90f6b311a66b4b0718fc71ea59
leeleelee3264 <absinthe4902@naver.com>
2024-07-03 21:32:34 +0900

    Test commit

---
+++ backend/schedule.py
@@ -0,0 +1,11 @@
+from enum import Enum
+import const
+
+
+class Character(Enum):
+    CARMILLA = const.KR_CARMILLA
+    LAURA = const.KR_LAURA
+    NICK = const.KR_NICK
+    SPIELSDORF = const.KR_SPIELSDORF
+
+
